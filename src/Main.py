dab9c70273d539b4e1150610b7307587c24004b4
Sebastian Ossner <github@sebastianoner.com>
2020-10-16 20:07:02 +0200

    Add dossier command and add option of requesting a weapon at signup

--- src/Main.py
+++ src/Main.py
@@ -1,22 +1,28 @@
-#Copyright 2020 Sebastian Ossner
+# Copyright 2020 Sebastian Ossner
 
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
 
-#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
 import logging
 import emoji
 import re
-from datetime import datetime
-from telegram import (ReplyKeyboardRemove, Bot)
+import random
+from telegram import (ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler)
+                          ConversationHandler, CallbackQueryHandler)
 
 from Game import (Game, checkPresent, checkStarted, checkStartable, checkJoinable, startGame, stopGame, underGameLimit, getPlayerlist, playerEnrolled)
-from Assassin import (Assassin, checkJoined, getPlayerCodeName, eliminatePlayer)
+from Assassin import (Assassin, checkJoined, getPlayerCodeName, eliminatePlayer, getTargetInfo)
 from DossierGen import compileDossier
 
 
@@ -26,7 +32,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 assassinDetails = {}
-GAMECODE, ASSASSINNAME, CODENAME, ADDRESS, MAJOR, PICTURELINK = range(6)
+GAMECODE, ASSASSINNAME, CODENAME, WEAPON, ADDRESS, MAJOR, PICTURE = range(7)
 
 
 # Fallback command if the user wants to end his
@@ -36,12 +42,14 @@ def cancel(update, context):
     update.message.reply_text('Cancelled. Roger')
     return ConversationHandler.END
 
+def start(update, context):
+    update.message.reply_text('Greetings aspiring assassin!\nI am M, your contact into the Secret Assassins Society. If you want to join a game of Assassins, type /joingame, if you want to start one yourself, type /newgame.\nIf you want to know more about me and what I can do, type /help')
+
 def new_game(update, context):
     if underGameLimit(update.message.chat_id):
         if not checkPresent(update.message.chat_id):
             game = Game(update.message.chat_id)
             os.mkdir('images\\' + str(game.id))
-            os.mkdir('dossiers\\' + str(game.id))
             update.message.reply_text('Alright. You will be the admin of game {}. Give this code to your players so they can register.'
                                         ' You can use /startgame to start this round'.format(game.id))
         else:
@@ -86,7 +94,6 @@ def broadcast(update, context):
     else:
         update.message.reply_text('You don\'t have a running game at the moment')
 
-
 def join_game(update, context):
     if checkJoined(update.message.chat_id):
         # TODO: If player is enrolled in a stopped game and not in a running game, simply update gameID and give player the chance to drop out if new signup is desired
@@ -108,25 +115,39 @@ def assassin_name(update, context):
 def code_name(update, context):
     assassinDetails['name'] = update.message.text;
     if dirty(assassinDetails['name']):
-        update.message.reply_text('🚨 Possible breach detected 🚨\n Please start over and refrain from using special characters')
+        update.message.reply_text('🚨 Possible breach detected 🚨\nPlease start over and refrain from using special characters')
         return ConversationHandler.END
     else:
         update.message.reply_text('Ok. Now tell me your way more interesting codename'.format(assassinDetails['name']))
         return CODENAME
 
-def address(update, context):
+def weapon(update, context):
     assassinDetails['codeName'] = update.message.text;
     if dirty(assassinDetails['codeName']):
-        update.message.reply_text('🚨 Possible breach detected 🚨\n Please start over and refrain from using special characters')
+        update.message.reply_text('🚨 Possible breach detected 🚨\nPlease start over and refrain from using special characters')
         return ConversationHandler.END
-    else:
-        update.message.reply_text('Affirmative {}. We also need your address for the life insurance form'.format(assassinDetails['codeName']))
-        return ADDRESS
+    
+    keyboard = [
+        [
+            InlineKeyboardButton('I need a weapon', callback_data=1),
+            InlineKeyboardButton('I have a weapon', callback_data=0)
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('To participate it is recommended that you own a hydro-pneumatic weapon. Our waterarms department can provide you with one if needed.', reply_markup=reply_markup)
+    return WEAPON
+
+def address(update, context):
+    query = update.callback_query
+    query.answer()
+    assassinDetails['weapon'] = query.data
+    query.edit_message_text('Affirmative {}. We also need your address for the life insurance form'.format(assassinDetails['codeName']))
+    return ADDRESS
 
 def major(update, context):
     assassinDetails['address'] = update.message.text
     if dirty(assassinDetails['address']):
-        update.message.reply_text('🚨 Possible breach detected 🚨\n Please start over and refrain from using special characters')
+        update.message.reply_text('🚨 Possible breach detected 🚨\nPlease start over and refrain from using special characters')
         return ConversationHandler.END
     else:
         update.message.reply_text('Almost done. Now I need to know your major')
@@ -135,22 +156,20 @@ def major(update, context):
 def image(update, context):
     assassinDetails['major'] = update.message.text
     if dirty(assassinDetails['major']):
-        update.message.reply_text('🚨 Possible breach detected 🚨\n Please start over and refrain from using special characters')
+        update.message.reply_text('🚨 Possible breach detected 🚨\nPlease start over and refrain from using special characters')
         return ConversationHandler.END
     else:
         update.message.reply_text('Lastly I need your pretty picture for your dossier')
-        return PICTURELINK
+        return PICTURE
 
-def signupDone(update, context):
+def signup_done(update, context):
     photo_file = update.message.photo[-1].get_file()
     chatId = update.message.chat_id
-    photo_file.download('images\\' + str(assassinDetails['gameId']) + '\\' + str(chatId) + '.jpg')
+    photo_file.download('images/{}/{}.jpg'.format(str(assassinDetails['gameId']), str(chatId)))
 
-    player = Assassin(assassinDetails['name'], assassinDetails['codeName'], assassinDetails['address'], chatId, assassinDetails['major'], assassinDetails['gameId'])
+    player = Assassin(assassinDetails['name'], assassinDetails['codeName'], assassinDetails['address'], chatId, assassinDetails['major'], assassinDetails['weapon'], assassinDetails['gameId'])
 
     update.message.reply_text('That\'s it. I will contact you again once the game has begun. Stay vigilant!')
-
-    compileDossier(player)
 
 def dropout(update, context):
     if checkJoined(update.message.chat_id):
@@ -171,6 +190,28 @@ def burn(update, context):
         update.message.reply_text('This is not a valid player-id')
     pass
 
+def dossier(update, context):
+    if (checkJoined(update.message.chat_id)):
+        targetInfo = getTargetInfo(update.message.chat_id)
+        random_skills = ['lockpicking', 'hand-to-hand combat', 'target acquisition',
+                 'covert operations', 'intelligence gathering', 'marksmanship',
+                 'knife-throwing', 'explosives', 'poison', 'seduction',
+                 'disguises', 'exotic weaponry', 'vehicles', 'disguise']
+        rand = random.randint(0, len(random_skills) - 1)
+        rand2 = random.randint(0, len(random_skills) - 1)
+        while rand == rand2:
+            rand2 = random.randint(0, len(random_skills) - 1)
+        
+        context.bot.send_photo(chat_id=update.message.chat_id, photo=open('images/' + str(targetInfo[1]) + '/' + str(targetInfo[0]) + '.jpg', 'rb'), caption=
+        'Name: ' + str(targetInfo[2]) + 
+        '\n\nCodename: ' + str(targetInfo[3]) + 
+        '\n\nAddress: ' + str(targetInfo[4]) + 
+        '\n\nSpeciality: ' + random_skills[rand]+', '+random_skills[rand2]+', '+str(targetInfo[5])+ 
+        '\n\nConsidered to be armed and extremely dangerous!')
+    else:
+        update.message.reply_text('You are not enrolled in a running game')
+
+
 def help_overview(update, context):
     update.message.reply_text(
         'This Bot was written by github.com/ossner. The code is licensed under the MIT license\n'
@@ -178,7 +219,6 @@ def help_overview(update, context):
         '/joingame - Join an upcoming game\n'
         '/dropout - Drop out of the game you\'re registered to\n'
         '/newgame - Create a new game of Assassins where you\'re the admin\n'
-        '/info - Get information about the game you\'re in\n'
         '/confirmkill - Confirm target elimination\n'
         '/dossier - Re-send your target\'s information\n'
         '/scoreboard - [ADMIN] Get the scoreboard of the game you\'re running\n'
@@ -192,6 +232,10 @@ def dirty(string):
 def main():
     updater = Updater(os.getenv("SAS_TOKEN"), use_context=True)
     dp = updater.dispatcher
+
+    start_handler = CommandHandler('start', start, run_async=True)
+    dp.add_handler(start_handler)
+
     newGameHandler = CommandHandler('newgame', new_game, run_async=True)
     dp.add_handler(newGameHandler)
 
@@ -204,17 +248,19 @@ def main():
     broadcast_handler = CommandHandler('broadcast', broadcast, run_async=True)
     dp.add_handler(broadcast_handler)
 
-    joinGame_handler = ConversationHandler(entry_points=[CommandHandler('joinGame', join_game, run_async=True)],
-                                          states={
-                                              # After getting key, you move into method in the value
-                                              GAMECODE: [MessageHandler(Filters.text, assassin_name, run_async=True)],
-                                              ASSASSINNAME: [MessageHandler(Filters.text, code_name, run_async=True)],
-                                              CODENAME: [MessageHandler(Filters.text, address, run_async=True)],
-                                              ADDRESS: [MessageHandler(Filters.text, major, run_async=True)],
-                                              MAJOR: [MessageHandler(Filters.text, image, run_async=True)],
-                                              PICTURELINK: [MessageHandler(Filters.photo, signupDone, run_async=True)]
-                                          },
-                                          fallbacks=[CommandHandler('cancel', cancel)])
+    joinGame_handler = ConversationHandler(
+        entry_points=[CommandHandler('joinGame', join_game, run_async=True)],
+        states={
+            # After getting key, you move into method in the value
+            GAMECODE: [MessageHandler(Filters.text, assassin_name, run_async=True)],
+            ASSASSINNAME: [MessageHandler(Filters.text, code_name, run_async=True)],
+            CODENAME: [MessageHandler(Filters.text, weapon, run_async=True)],
+            WEAPON: [CallbackQueryHandler(address, pattern='^\d$' , run_async=True)],
+            ADDRESS: [MessageHandler(Filters.text, major, run_async=True)],
+            MAJOR: [MessageHandler(Filters.text, image, run_async=True)],
+            PICTURE: [MessageHandler(Filters.photo, signup_done, run_async=True)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel, run_async=True)])
     dp.add_handler(joinGame_handler)
 
     dropout_handler = CommandHandler('dropout', dropout, run_async=True)
@@ -222,6 +268,9 @@ def main():
 
     burn_handler = CommandHandler('burn', burn, run_async=True)
     dp.add_handler(burn_handler)
+
+    dossier_handler = CommandHandler('dossier', dossier, run_async=True)
+    dp.add_handler(dossier_handler)
 
     #TODO: Leaderboard, ConfirmKill, ConfirmDead, Dossier
 
