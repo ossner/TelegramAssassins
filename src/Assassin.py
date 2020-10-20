f7118bfb81eac0bc74df774ec5000283889fec41
Sebastian Ossner <github@sebastianoner.com>
2020-10-20 13:13:36 +0200

    remove stopped references from game as game deletion is now cascading

--- src/Assassin.py
+++ src/Assassin.py
@@ -10,26 +10,33 @@ connection = mysql.connector.connect(
 cursor = connection.cursor(buffered=True)
 
 class Assassin:
-    def __init__(self, name, codename, address, chat_id, major, idgame):
+    def __init__(self, name, codename, address, chat_id, major, weapon, idgame):
         self.name = name
         self.codename = codename
         self.address = address
         self.chat_id = chat_id
         self.major = major
+        self.weapon = weapon
         self.idgame = idgame
         self.enterData()
 
     def enterData(self):
-        cursor.execute("INSERT INTO ASSASSINS (`id`, `first_name`, `code_name`, `address`, `major`, `game`) VALUES (%s, %s, %s, %s, %s, %s)", (self.chat_id, self.name, self.codename, self.address, self.major, self.idgame))
+        cursor.execute("INSERT INTO ASSASSINS (`id`, `first_name`, `code_name`, `address`, `major`, `needs_weapon`, `game`)"
+        "VALUES (%s, %s, %s, %s, %s, %s, %s)", (self.chat_id, self.name, self.codename, self.address, self.major, self.weapon, self.idgame))
         connection.commit()
 
-def checkJoined(assassin_id):
-    cursor.execute("SELECT * from assassins INNER JOIN games on games.id=assassins.game AND games.stopped=0 AND assassins.id=%s", (assassin_id,))
+# Returns a result if the player with the given id has joined a game that is either upcoming or already started
+def checkJoined(assassin_id, started=False):
+    if started:
+        cursor.execute("SELECT * from assassins INNER JOIN games on games.id=assassins.game AND games.started=1 AND assassins.id=%s", (assassin_id,))
+    else:
+        cursor.execute("SELECT * from assassins INNER JOIN games on games.id=assassins.game AND assassins.id=%s", (assassin_id,))
     connection.commit()
     return cursor.fetchone()
 
 def getPlayerCodeName(assassin_id, master_id):
-    cursor.execute("SELECT assassins.code_name FROM assassins INNER JOIN games g ON assassins.game = g.id AND g.master=%s AND assassins.id=%s AND g.started=1", (master_id, assassin_id))
+    cursor.execute("SELECT assassins.code_name FROM assassins INNER JOIN games g ON assassins.game = g.id"
+    "AND g.master=%s AND assassins.id=%s AND g.started=1", (master_id, assassin_id))
     connection.commit()
     return cursor.fetchone()
 
--- src/Game.py
+++ src/Game.py
@@ -13,53 +13,47 @@ cursor = connection.cursor(buffered=True)
 class Game:
     def __init__(self, master):
         self.master = master
-        self.id = random.randint(100000, 999999) # creates a game id that might not be unique. it would be easy enough to check if it's taken,
+        self.id = random.randint(100000, 999999) # creates a random 6 digit game id that might not be unique. it would be easy enough to check if it's taken,
                                                  # but I like a little danger in my life
         self.enterData()
 
 
     def enterData(self):
-        cursor.execute("INSERT INTO Games (id, master) VALUES (%s, %s);",(self.id, self.master))
+        cursor.execute("INSERT INTO games (id, master) VALUES (%s, %s);", (self.id, self.master))
         connection.commit()
 
 
-# checks whether or not the user with the id already registered a game in progress
-def checkPresent(master):
-    cursor.execute("SELECT master FROM Games where master=%s AND stopped=0 LIMIT 1", (master, ))
-    connection.commit()
+# checks whether or not the user with the id already registered a game
+def checkPresent(master, started=False):
+    if started:
+        cursor.execute("SELECT master FROM games WHERE master=%s AND started=1;", (master, ))
+    else:
+        cursor.execute("SELECT master FROM games where master=%s;", (master, ))
+        connection.commit()
     return cursor.fetchone()
 
-
-# checks if the user with this id already has 10 past games in the database, if so return false
-def underGameLimit(master):
-    cursor.execute("SELECT id FROM Games where master=%s", (master, ))
-    connection.commit()
-    return len(cursor.fetchall()) < 10
-
-
-
-def checkStarted(master):
-    cursor.execute("SELECT master FROM Games where master=%s AND started=1 LIMIT 1", (master, ))
-    connection.commit()
-    return cursor.fetchone()
-
-# Checks if there is a game by this gamemaster which is not stopped
 def checkStartable(master):
-    cursor.execute("SELECT master FROM Games where master=%s AND stopped=0 LIMIT 1", (master, ))
+    cursor.execute("SELECT master FROM games where master=%s AND started=0;", (master, ))
     connection.commit()
     return cursor.fetchone()
-
 
 def startGame(master):
-    cursor.execute("UPDATE Games SET started=1 WHERE master=%s AND stopped=0;", (master, ))
+    cursor.execute("UPDATE games SET started=1 WHERE master=%s;", (master, ))
     connection.commit()
-    # TODO: distribute targets, send out dossiers
-    # TODO: check if min player number has been reached of if game has already started, return false
+    return assignTargets(master)
+
+def assignTargets(master):
+    playerIds = getPlayerlist(master)
+    for i in range(len(playerIds)):
+        cursor.execute("UPDATE assassins SET target=%s WHERE id=%s;", ((playerIds[(i+1)%len(playerIds)][0]), playerIds[i][0]))
+    connection.commit()
+    return playerIds
 
 def stopGame(master):
-    cursor.execute("UPDATE Games SET started=0, stopped=1 WHERE master=%s;", (master, ))
-    connection.commit()
     # TODO: send out leaderboard and notify players
+    # deleting the game will cascade and delete all assassins registered to that game
+    cursor.execute("DELETE FROM games WHERE master=%s;", (master, ))
+    connection.commit()
 
 # Send out leaderboard of game
 def leaderboard(master, redacted):
@@ -68,17 +62,22 @@ def leaderboard(master, redacted):
 
 # retrieves all players currently enrolled in this masters running game
 def getPlayerlist(master):
-    cursor.execute("SELECT assassins.id from assassins INNER JOIN games on games.id=assassins.game AND games.master=%s AND games.started=1", (master, ))
+    cursor.execute("SELECT assassins.id from assassins INNER JOIN games on games.id=assassins.game AND games.master=%s AND games.started=1;", (master, ))
     connection.commit()
     return cursor.fetchall()
 
+def getMaster(game_id):
+    cursor.execute("SELECT games.master FROM games WHERE id=%s", (game_id, ))
+    connection.commit()
+    return cursor.fetchone()
+
 
 def checkJoinable(id):
-    cursor.execute("SELECT * from games WHERE id=%s AND started=0 AND stopped=0", (id, ))
+    cursor.execute("SELECT * from games WHERE id=%s AND started=0;", (id, ))
     connection.commit()
     return cursor.fetchone()
 
 def playerEnrolled(player_id, master_id):
-    cursor.execute("SELECT assassins.code_name FROM assassins INNER JOIN games g ON assassins.game = g.id AND g.master=%s AND assassins.id=%s AND g.started=1", (master_id, player_id))
+    cursor.execute("SELECT assassins.code_name FROM assassins INNER JOIN games g ON assassins.game = g.id AND g.master=%s AND assassins.id=%s AND g.started=1;", (master_id, player_id))
     connection.commit()
     return cursor.fetchone()
